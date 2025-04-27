bdd426118e9238548f6d62036815bb1294b240f7
SofiMartin <mariasofiamartin@hotmail.com>
2025-04-27 15:36:48 -0400

    add lists and update index.js

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
+const movieRoute = require('./routes/movies');
+const listRoute = require('./routes/lists');
 
 dotenv.config();
 
@@ -15,6 +17,8 @@ mongoose.connect(process.env.MONGO_URL)
 
   app.use("/api/auth", authRoute);
   app.use("/api/users", userRoute);
+  app.use("/api/movies", movieRoute);
+  app.use("/api/lists", listRoute);
 
 app.listen(8800, () => {
   console.log('Server is running on port 8800');
